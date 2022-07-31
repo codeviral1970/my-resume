a9e71f0dfed1ebaa49211b64e2c4080e13a27639
Man <manbanhduc@gmail.com>
2022-07-31 06:17:41 +0200

    Initial commit portfolio with taildwin css

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function updateList() {
+	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+	});
+
+	document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+	
+	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+}
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+})
+
+const btn = document.querySelector("button.mobile-menu-button");
+
+const mobile_menu = document.querySelector('.mobile-menu');
+
+btn.addEventListener('click', () => {
+    mobile_menu.classList.toggle('hidden');
+});
+
+mobile_menu.addEventListener('click', () => {
+    mobile_menu.classList.add('hidden');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: [
+    './templates/**/*.html.twig',
+    './assets/**/*.css',
+    './assets/**/*.js',
+  ],
+  content: [],
+  theme: {
+      extend: {
+        colors: {
+            'body': '#17171F',
+            'selected-text': '#FAC748',
+            'theme': '#FAC748',
+            'secondary': '#9191A4',
+            'badge': '#3F3F51',
+            'inut-border': '#565666',
+            'input': '#2A2A35'
+          },
+            fontFamily: {
+                'poppins': ["'poppins'", 'sans-serif']
+          },
+         
+    },
+  },
+  plugins: [],
+}
+
+//FAC748
+//'body': '#17171F',
+//'theme': '#3F3FFF',
